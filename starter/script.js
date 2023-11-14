380e5a107564665e23aaf7e46e8b2162a85534cd
Hopeson <hopesonogaga@gmail.com>
2023-11-14 13:01:51 +0100

    event listeners

--- starter/script.js
+++ starter/script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, map_event;
 // gealocation api
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
@@ -23,26 +24,57 @@ if (navigator.geolocation)
         `https://www.google.com/maps/@${latitude},${longitude},6z?entry=ttu`
       );
       //  L.map('map') this refers to the id
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
+      // handling clicks om map
+      map.on('click', function (map_e) {
+        map_event = map_e;
+        form.classList.remove('hidden');
+        inputDistance.focus();
 
-      map.on('click', function (map_event) {
         console.log(map_event);
-        const { lat } = map_event.latlng;
-        const { lng } = map_event.latlng;
-        const coords = [lat, lng]
-        console.log(lat,lng);
-        L.marker(coords)
-          .addTo(map)
-          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-          .openPopup();
       });
     },
     function () {
       alert('could not get current location');
     }
   );
+
+// .running-popup
+// .cycling-popup
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  //display marker
+  const { lat } = map_event.latlng;
+  const { lng } = map_event.latlng;
+  const coords = [lat, lng];
+  console.log(lat, lng);
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
